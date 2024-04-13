0ee8bcc7e4a9b59bdf402a75923e43700030bda8
Rogpe <paulo.rogpe@gmail.com>
2024-04-13 18:10:21 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/hero/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.hero.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hero.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
